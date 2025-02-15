8b7899d58aad4e76d116c0a8b9d1820e2f8cee1e
Mohamed Sharif <135287156+mSharifHub@users.noreply.github.com>
2025-02-15 13:54:42 -0800

    adding a folder component

---
+++ app/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import  React from "react";
+
+interface FooterProps {
+    children?: React.ReactNode
+}
+
+
+export const Footer: React.FC<FooterProps> = ({children}) => {
+
+    return (
+
+        <div>
+            skeleton div
+
+        </div>
+    )
+}
