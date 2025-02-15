0ee03a6c75e7db77e6baa92f76c0f3299f746a59
William-Ta0 <106932720+William-Ta0@users.noreply.github.com>
2025-02-15 14:42:46 -0800

    backend basic structure
    backend localhost and mongodb setup.

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = process.env.MONGO_URL;
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server! This is the root route.');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log(`Connected to MongoDB database: ${mongoose.connection.db.databaseName}`);
+}).catch((err) => {
+    console.error('MongoDB Connection Error Details:');
+    console.error('Error name:', err.name);
+    console.error('Error message:', err.message);
+    console.error('Error code:', err.code);
+    process.exit(1);
+});
+
+
+module.exports = app;
